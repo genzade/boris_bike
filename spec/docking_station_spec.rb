75880a1eec815dc1e181b97eeaa26f7035fcd61f
Hamid Zade <hamidmamadzade@gmail.com>
2015-11-25 17:35:42 +0000

    step 11 and 12

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,9 +3,26 @@ require 'docking_station'
 describe DockingStation do
 	it { is_expected.to respond_to :release_bike }
 
-	it 'get bike and working' do
-		bike = subject.release_bike
-		expect(bike).to be_working
-	end
-end
+	describe '#release_bike' do	
+		it 'release bike' do
+			bike = Bike.new
+			subject.dock(bike)
+			expect(subject.release_bike).to eq bike
+		end
 
+		it 'raises error when there are no bikes' do
+			expect { subject.release_bike }.to raise_error 'No bikes available'
+		end
+	end
+
+	it 'is bike docked' do
+		bike = Bike.new
+		subject.dock(bike)
+		expect(subject.bike).to eq bike
+	end
+
+#	it 'no bikes' do
+#		allow(subject).to receive(:bike) {nil}
+#		expect(subject.release_bike(bike)).to raise_error("no bikes available")
+#	end
+end
---
+++ spec/feature_spec.rb
@@ -0,0 +1,5 @@
+require '/Users/zade/Desktop/Projects/boris_bike/lib/docking_station.rb'
+
+docking_station = DockingStation.new
+
+docking_station.release_bike
