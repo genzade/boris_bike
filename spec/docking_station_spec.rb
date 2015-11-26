84ce3aa7177fae03909d51e2d043c23707305cb4
Hamid Zade <hamidmamadzade@gmail.com>
2015-11-26 13:02:20 +0000

    step 13 and 14

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -15,14 +15,15 @@ describe DockingStation do
 		end
 	end
 
+  it 'raises error when docking station is full' do
+    20.times { subject.dock(Bike.new) }
+    expect { subject.dock(Bike.new) }.to raise_error 'station is full'
+  end
+
 	it 'is bike docked' do
 		bike = Bike.new
 		subject.dock(bike)
-		expect(subject.bike).to eq bike
+		expect(subject.release_bike).to eq bike
 	end
 
-#	it 'no bikes' do
-#		allow(subject).to receive(:bike) {nil}
-#		expect(subject.release_bike(bike)).to raise_error("no bikes available")
-#	end
 end
--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -2,4 +2,4 @@ require '/Users/zade/Desktop/Projects/boris_bike/lib/docking_station.rb'
 
 docking_station = DockingStation.new
 
-docking_station.release_bike
+20.times { docking_station.dock Bike.new }
